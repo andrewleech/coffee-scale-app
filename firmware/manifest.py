07a6d2ce2a16747cde140a89d6c5b031a56d9ff6
Andrew Leech <andrew.leech@planetinnovation.com.au>
2022-01-24 11:40:38 +1100

    firmware: Add manifest file.

---
+++ firmware/manifest.py
@@ -0,0 +1,11 @@
+freeze(".", (
+    "art.py",
+    "ble_advertising.py",
+    "ble_scales.py",
+    "boot.py",
+    "config_def.py",
+    "filtering.py",
+    "hx711_spi.py",
+    "main.py",
+    "ssd1306.py",
+), opt=0)
