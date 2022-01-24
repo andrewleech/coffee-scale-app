9a99ea6aaf4fba5cc8bafd834e0107288ed6142b
Andrew Leech <andrew.leech@planetinnovation.com.au>
2022-01-24 12:32:21 +1100

    firmware: Migrate pin settings to separate config file.

--- firmware/config_def.py
+++ firmware/config_def.py
@@ -7,14 +7,14 @@ try:
 except ImportError:
     pass
 
-SCREEN_I2C_SDA = 21  # GPIO
-SCREEN_I2C_SCL = 22  # GPIO
+SCREEN_I2C_SDA = 21
+SCREEN_I2C_SCL = 22
 
 HX711_GAIN = 64
 HX711_0_SPI_ID = 1  # SPI interface to use
-HX711_0_DOUT = 12   # Connected to DOUT pin on HX711
-HX711_0_CLK = 13    # Connected to SCK pin on HX711
-HX711_0_SSCK = 14   # Dummy pin on ESP, not connected
+HX711_0_DOUT = 32   # Connected to DOUT pin on HX711
+HX711_0_CLK = 33    # Connected to SCK pin on HX711
+HX711_0_SSCK = 25   # Dummy pin on ESP, not connected
 
 BUTTON_PIN = 0
 
